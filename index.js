df6f24f1c7e099e7f614a7443f6e805990b6e822
Archit Dhiman <archit210219@gmail.com>
2024-06-01 18:47:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function (){
+    const gridSize = 9;
+    const solveButton = document.getElementById("solve-puzzle");
+    solveButton.addEventListener('click', solvePuzzle);
+})
